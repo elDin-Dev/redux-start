f5fc8b9e94a62a89a297e3895c5347b12b5ab9eb
elDin-Dev <alexmaildese-dev@yahoo.com>
2020-04-30 11:32:23 +0200

    dispatching actions from componentsç

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,7 +3,15 @@ const initialState = {
     counter: 0,
 }
 
-const reducer = (state= initialState, action) => {
+const reducer = (state = initialState, action) => {
+
+    console.log('[reducer:]',action)
+    if (action.type === 'INCREMENT') {
+        return {
+            counter: state.counter + 1,
+        }
+    }
+
     return state;
 }
 
