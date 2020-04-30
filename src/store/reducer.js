0121de8a9325789a618eb9513df9a3cd1b4a80c4
elDin-Dev <alexmaildese-dev@yahoo.com>
2020-04-30 11:46:43 +0200

    reducing with switch

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -5,26 +5,29 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
 
-    console.log('[reducer:]',action)
-    if (action.type === 'INCREMENT') {
-        return {
-            counter: state.counter + 1,
-        }
-    }
-    if (action.type === 'DECREMENT') {
-        return {
-            counter: state.counter - 1,
-        }
-    }
-    if (action.type === 'ADD') {
-        return {
-            counter: state.counter + action.val,
-        }
-    }
-    if (action.type === 'SUBTRACT') {
-        return {
-            counter: state.counter - action.val,
-        }
+    console.log('[reducer:]', action)
+
+    switch (action.type) {
+        case 'INCREMENT':
+            return {
+                counter: state.counter + 1,
+            }
+
+        case 'DECREMENT':
+            return {
+                counter: state.counter - 1,
+            }
+        case 'ADD':
+            return {
+                counter: state.counter + action.val,
+            };
+
+        case 'SUBTRACT':
+            return {
+                counter: state.counter - action.val,
+            };
+
+
     }
 
     return state;
