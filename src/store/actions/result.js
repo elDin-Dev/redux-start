4ccdb71bf03c65d03a2b659be788cfc3834c8d9e
elDin-Dev <alexmaildese-dev@yahoo.com>
2020-05-04 13:45:42 +0200

    getting state on actions creators

--- src/store/actions/result.js
+++ src/store/actions/result.js
@@ -12,8 +12,10 @@ export const saveResult = (res) => {
 
 export const storeResult = (res) => {
 
-    return dispatch => {
+    return (dispatch, getState) => {
         setTimeout( () => {
+            const oldCounter= getState().ctr.counter;
+            console.log(oldCounter);
             dispatch(saveResult(res))
         },2000);
     }
