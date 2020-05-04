941ae1f79e396ac76f090be493bff6f541c91715
elDin-Dev <alexmaildese-dev@yahoo.com>
2020-05-04 12:00:51 +0200

    adding async code on reducer store

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import {createStore, combineReducers, applyMiddleware, compose} from 'redux'
 import counterReducer from './store/reducers/counter';
 import resultReducer from './store/reducers/result';
 import {Provider } from 'react-redux'
+import thunk from 'redux-thunk';
 
 const rootReducer = combineReducers({
     ctr: counterReducer,
@@ -26,7 +27,7 @@ const logger = store => {
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(rootReducer, composeEnhancers( applyMiddleware(logger)));
+const store = createStore(rootReducer, composeEnhancers( applyMiddleware(logger, thunk)));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -32,11 +32,20 @@ export const subtract = (value) => {
     };
 };
 
-export const storeResult = (res) => {
-    return {
+export const saveResult = (res) => {
+    return  {
         type: STORE_RESULT,
         result:res,
     };
+}
+
+export const storeResult = (res) => {
+
+    return dispatch => {
+        setTimeout( () => {
+            dispatch(saveResult(res))
+        },2000);
+    }
 };
 
 export const deleteResult = (resElId) => {
